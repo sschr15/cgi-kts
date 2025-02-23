3799e4591db10d83e42f9884e0b91d7afd6ca99c
sschr15 <steven.sschr15@hotmail.com>
2025-02-23 17:11:49 -0600

    Fix some weirdness

--- scripting-host/build.gradle.kts
+++ scripting-host/build.gradle.kts
@@ -17,3 +17,12 @@ java {
 application {
     mainClass = "com.sschr15.scripting.DaemonMain"
 }
+
+tasks.jar {
+    manifest { 
+        attributes["Main-Class"] = application.mainClass
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
