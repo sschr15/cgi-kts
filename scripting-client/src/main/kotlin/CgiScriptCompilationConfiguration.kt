fc372d3d40bbec90b29d5d0784365b5197a74c75
sschr15 <steven.sschr15@hotmail.com>
2025-04-11 22:33:50 -0500

    Fix for repeatable, presumably

--- scripting-client/src/main/kotlin/CgiScriptCompilationConfiguration.kt
+++ scripting-client/src/main/kotlin/CgiScriptCompilationConfiguration.kt
@@ -65,114 +65,147 @@ internal object CgiScriptCompilationConfiguration : ScriptCompilationConfigurati
     }
 
     private fun handleMavenCentral(context: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
-        val source = context.collectedData?.get(ScriptCollectedData.collectedAnnotations)
-            ?.firstOrNull { it.annotation is DependsOnMavenCentral }
+        val sources = context.collectedData?.get(ScriptCollectedData.collectedAnnotations)
+            ?.filter { it.annotation is DependsOnMavenCentral }
             ?: return context.compilationConfiguration.asSuccess()
 
-        val mvnCentral = source.annotation as DependsOnMavenCentral
+        var config = context.compilationConfiguration
 
-        data class Dep(val group: String, val artifact: String, val version: String, val classifier: String, val extension: String)
-        val dep = if (mvnCentral.coordinates.isNotBlank()) {
-            val match = artifactRegex.matchEntire(mvnCentral.coordinates) ?: return fail("Invalid coordinates: ${mvnCentral.coordinates}", source.location)
-            val group = match.groups["group"]!!.value.replace('.', '/')
-            val name = match.groups["name"]!!.value
-            val version = match.groups["version"]!!.value
-            val classifier = match.groups["classifier"]?.value
-            val extension = match.groups["extension"]?.value ?: "jar"
+        for (source in sources) {
+            val mvnCentral = source.annotation as DependsOnMavenCentral
 
-            val classifierPart = classifier?.let { "-$it" } ?: ""
+            data class Dep(
+                val group: String,
+                val artifact: String,
+                val version: String,
+                val classifier: String,
+                val extension: String
+            )
 
-            Dep(group, name, version, classifierPart, extension)
-        } else {
-            if (mvnCentral.group.isBlank()) return fail("No specified MavenCentral group", source.location)
-            if (mvnCentral.artifact.isBlank()) return fail("No specified MavenCentral artifact", source.location)
-            if (mvnCentral.version.isBlank()) return fail("No specified MavenCentral version", source.location)
+            val dep = if (mvnCentral.coordinates.isNotBlank()) {
+                val match = artifactRegex.matchEntire(mvnCentral.coordinates)
+                    ?: return fail("Invalid coordinates: ${mvnCentral.coordinates}", source.location)
+                val group = match.groups["group"]!!.value.replace('.', '/')
+                val name = match.groups["name"]!!.value
+                val version = match.groups["version"]!!.value
+                val classifier = match.groups["classifier"]?.value
+                val extension = match.groups["extension"]?.value ?: "jar"
 
-            val group = mvnCentral.group.replace('.', '/')
-            val name = mvnCentral.artifact
-            val version = mvnCentral.version
-            val classifier = mvnCentral.classifier.takeIf { it.isNotBlank() }?.let { "-$it" } ?: ""
-            val extension = mvnCentral.extension
+                val classifierPart = classifier?.let { "-$it" } ?: ""
 
-            Dep(group, name, version, classifier, extension)
-        }
+                Dep(group, name, version, classifierPart, extension)
+            } else {
+                if (mvnCentral.group.isBlank()) return fail("No specified MavenCentral group", source.location)
+                if (mvnCentral.artifact.isBlank()) return fail(
+                    "No specified MavenCentral artifact",
+                    source.location
+                )
+                if (mvnCentral.version.isBlank()) return fail("No specified MavenCentral version", source.location)
 
-        val (group, name, version, classifier, extension) = dep
-        val path = "$group/$name/$version/$name-$version$classifier.$extension"
-        val url = "$MAVEN_CENTRAL_URL/$path"
-        val resultLocation = Path(System.getenv("HOME"), ".m2/repository", path)
-        val result = runCatching {
-            if (resultLocation.exists()) {
-                val sha1 = MessageDigest.getInstance("SHA-1").apply { 
-                    resultLocation.inputStream().use {
-                        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
-                        var bytesRead = it.read(buffer)
-                        while (bytesRead != -1) {
-                            update(buffer, 0, bytesRead)
-                            bytesRead = it.read(buffer)
+                val group = mvnCentral.group.replace('.', '/')
+                val name = mvnCentral.artifact
+                val version = mvnCentral.version
+                val classifier = mvnCentral.classifier.takeIf { it.isNotBlank() }?.let { "-$it" } ?: ""
+                val extension = mvnCentral.extension
+
+                Dep(group, name, version, classifier, extension)
+            }
+
+            val (group, name, version, classifier, extension) = dep
+            val path = "$group/$name/$version/$name-$version$classifier.$extension"
+            val url = "$MAVEN_CENTRAL_URL/$path"
+            val resultLocation = Path(System.getenv("HOME"), ".m2/repository", path)
+            val result = runCatching {
+                if (resultLocation.exists()) {
+                    val sha1 = MessageDigest.getInstance("SHA-1").apply {
+                        resultLocation.inputStream().use {
+                            val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+                            var bytesRead = it.read(buffer)
+                            while (bytesRead != -1) {
+                                update(buffer, 0, bytesRead)
+                                bytesRead = it.read(buffer)
+                            }
                         }
+                    }.digest().toHexString()
+
+                    logger.debug { "sha1 of cached artifact $path: $sha1" }
+
+                    val foundSha1 = URI.create("$url.sha1").toURL().openStream().readAllBytes().decodeToString()
+                    if (sha1 == foundSha1) {
+                        logger.debug { "Cached artifact $path matches sha1 checksum" }
+                        return@runCatching
+                    } else {
+                        logger.debug { "Cached artifact $path does not match sha1 checksum (MavenCentral reports $foundSha1)" }
+                        resultLocation.deleteExisting()
                     }
-                }.digest().toHexString()
+                }
 
-                logger.debug { "sha1 of cached artifact $path: $sha1" }
-
-                val foundSha1 = URI.create("$url.sha1").toURL().openStream().readAllBytes().decodeToString()
-                if (sha1 == foundSha1) {
-                    logger.debug { "Cached artifact $path matches sha1 checksum" }
-                    return@runCatching
-                } else {
-                    logger.debug { "Cached artifact $path does not match sha1 checksum (MavenCentral reports $foundSha1)" }
-                    resultLocation.deleteExisting()
+                resultLocation.parent.createDirectories()
+                URI.create(url).toURL().openStream().use {
+                    Files.copy(it, resultLocation, StandardCopyOption.REPLACE_EXISTING)
                 }
             }
 
-            resultLocation.parent.createDirectories()
-            URI.create(url).toURL().openStream().use {
-                Files.copy(it, resultLocation, StandardCopyOption.REPLACE_EXISTING)
+            if (result.isSuccess) {
+                config = config.with {
+                    dependencies.append(JvmDependency(resultLocation.toFile()))
+                }
+            } else {
+                return fail(
+                    "Failed to download artifact from $url",
+                    source.location,
+                    result.exceptionOrNull()
+                        ?: NullPointerException("result.exceptionOrNull() returned null despite the result not being a success")
+                )
             }
         }
-        return if (result.isSuccess) {
-            context.compilationConfiguration.with {
-                dependencies.append(JvmDependency(resultLocation.toFile()))
-            }.asSuccess()
-        } else {
-            fail("Failed to download artifact from $url", source.location, result.exceptionOrNull() ?: NullPointerException("result.exceptionOrNull() returned null despite the result not being a success"))
-        }
+
+        return config.asSuccess()
     }
 
     private fun handleFile(context: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
-        val source = context.collectedData?.get(ScriptCollectedData.collectedAnnotations)
-            ?.firstOrNull { it.annotation is DependsOnLocal }
+        val sources = context.collectedData?.get(ScriptCollectedData.collectedAnnotations)
+            ?.filter { it.annotation is DependsOnMavenCentral }
             ?: return context.compilationConfiguration.asSuccess()
 
-        val local = source.annotation as DependsOnLocal
-        val file = Path(local.path)
-        if (!file.exists()) return fail("File does not exist: ${file.toAbsolutePath()}", source.location)
-        return context.compilationConfiguration.with {
-            dependencies.append(JvmDependency(file.toFile()))
-        }.asSuccess()
+        var config = context.compilationConfiguration
+        for (source in sources) {
+            val local = source.annotation as DependsOnLocal
+            val file = Path(local.path)
+            if (!file.exists()) return fail("File does not exist: ${file.toAbsolutePath()}", source.location)
+            config = config.with {
+                dependencies.append(JvmDependency(file.toFile()))
+            }
+        }
+
+        return config.asSuccess()
     }
 
     private fun handleUrl(context: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
-        val source = context.collectedData?.get(ScriptCollectedData.collectedAnnotations)
-            ?.firstOrNull { it.annotation is DependsOnUrl }
+        val sources = context.collectedData?.get(ScriptCollectedData.collectedAnnotations)
+            ?.filter { it.annotation is DependsOnMavenCentral }
             ?: return context.compilationConfiguration.asSuccess()
 
-        val url = (source.annotation as DependsOnUrl).url.let { URI.create(it).toURL() }
-        val resultLocation = Path(System.getenv("HOME"), ".cgi-cache", url.path)
-        val result = runCatching {
-            resultLocation.parent.createDirectories()
-            url.openStream().use {
-                Files.copy(it, resultLocation)
+        var config = context.compilationConfiguration
+        for (source in sources) {
+            val url = (source.annotation as DependsOnUrl).url.let { URI.create(it).toURL() }
+            val resultLocation = Path(System.getenv("HOME"), ".cgi-cache", url.path)
+            val result = runCatching {
+                resultLocation.parent.createDirectories()
+                url.openStream().use {
+                    Files.copy(it, resultLocation)
+                }
+            }
+            if (result.isSuccess) {
+                config = config.with {
+                    dependencies.append(JvmDependency(resultLocation.toFile()))
+                }
+            } else {
+                return fail("Failed to download artifact from $url", source.location, result.exceptionOrNull())
             }
         }
-        return if (result.isSuccess) {
-            context.compilationConfiguration.with {
-                dependencies.append(JvmDependency(resultLocation.toFile()))
-            }.asSuccess()
-        } else {
-            fail("Failed to download artifact from $url", source.location, result.exceptionOrNull())
-        }
+
+        return config.asSuccess()
     }
 
     private fun fail(text: String, location: SourceCode.LocationWithId? = null, exception: Throwable? = null): ResultWithDiagnostics.Failure {
